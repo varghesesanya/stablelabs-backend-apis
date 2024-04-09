b679f453691fcd2921af21767c26bf3170b6b5f9
Sanya Varghese <varghese.sanya@gmail.com>
2024-04-09 21:13:29 +0530

    Add User Entity and Interface

---
+++ src/user/model/user.entity.ts
@@ -0,0 +1,25 @@
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class UserEntity {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({unique: true})
+  username: string;
+
+  @Column({unique: true})
+  email: string;
+
+  @Column({select: false})
+  password: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  emailToLowerCase() {
+    this.email = this.email.toLowerCase();
+    this.username = this.username.toLowerCase();
+  }
+
+}
