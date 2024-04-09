90df6aff8efede172126be3778a6fab066f2c677
Sanya Varghese <varghese.sanya@gmail.com>
2024-04-10 00:08:02 +0530

    Route Calls to local Mongo DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,20 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(),
-    UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+    type: 'mongodb',
+    url: process.env.DATABASE_URL,
+    autoLoadEntities: true,
+    synchronize: true
+  }),
+  UserModule,
+  AuthModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
